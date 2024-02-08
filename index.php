4cdaa91a4c352080ee7bd0f67bd8e9510972a75c
angel de jesus <a20210014@utem.edu.mx>
2024-02-07 19:11:04 -0600

    cambios2

--- index.php
+++ index.php
@@ -10,31 +10,8 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="http://localhost/busca/Trabajos/">Inicio</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="http://localhost/busca/Trabajos/marcas.html">Marcas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="http://localhost/busca/Trabajos/acerca.html">Acerca de</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search" onsubmit="return highlight(event)">
-                    <input class="form-control me-2" type="search" id="searchInput" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
-        <center>
+    <?php include './navbar/navbar.php'; ?>
+    <center>
         <h1>Ratones Gaming</h1>
     </center>
     <div class="container-xxl">
